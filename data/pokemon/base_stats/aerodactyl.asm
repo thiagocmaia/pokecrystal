005b99e381f9e6874426d01c0670459145817455
thiagocmaia <75283851+thiagocmaia@users.noreply.github.com>
2023-03-06 07:58:38 -0300

    Update aerodactyl.asm

--- data/pokemon/base_stats/aerodactyl.asm
+++ data/pokemon/base_stats/aerodactyl.asm
@@ -1,19 +1,19 @@
 	db AERODACTYL ; 142
 
-	db  80, 105,  65, 130,  60,  75
+	db  140, 117,  132, 187,  117,  132
 	;   hp  atk  def  spd  sat  sdf
 
 	db ROCK, FLYING ; type
 	db 45 ; catch rate
 	db 202 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F12_5 ; gender ratio
+	db GENDER_UNKNOWN ; gender ratio
 	db 100 ; unknown 1
 	db 35 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/aerodactyl/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_SLOW ; growth rate
+	db GROWTH_FAST ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
 
 	; tm/hm learnset
